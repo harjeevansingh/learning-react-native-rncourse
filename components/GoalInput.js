4cf6bd31128e353058a702ee28bc07e6b412f289
Harjeevan Singh <harjeevansingh01@gmail.com>
2024-01-20 03:37:50 +0530

    feat: addesd modal image

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,4 @@
-import { StyleSheet, TextInput, View, Button, Modal } from 'react-native';
+import { StyleSheet, TextInput, View, Button, Modal, Image } from 'react-native';
 import React, { useState } from 'react';
 
 const GoalInput = ({ addGoalHandler, visible, closeModal }) => {
@@ -16,6 +16,7 @@ const GoalInput = ({ addGoalHandler, visible, closeModal }) => {
 	return (
 		<Modal visible={visible} animationType="slide">
 			<View style={styles.inputContainer}>
+                <Image source={require('../assets/input-background.png')} style={styles.inputImage} />
 				<TextInput
 					value={goalText}
 					style={styles.textInput}
@@ -46,6 +47,7 @@ const styles = StyleSheet.create({
         marginHorizontal:16,
 	},
 	textInput: {
+        marginTop: 16,
 		borderWidth: 1,
 		borderColor: '#cccccc',
 		width: '100%',
@@ -60,5 +62,9 @@ const styles = StyleSheet.create({
     button:{
         width:'40%',
         marginHorizontal:8
-    }
+    },
+    inputImage:{
+        width:100,
+        height:100,
+    },
 });
